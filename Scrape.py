fdc91814cada6e41b160f4e5de62187b08560bd0
jamm <jamm@iastate.edu>
2021-10-23 17:37:10 -0500

    Scrape.py now prints the title and link of every post on the front page of HN

--- Scrape.py
+++ Scrape.py
@@ -1,12 +1,36 @@
 import re
 from urllib.request import urlopen
 
+class FoundThing:
+    def __init__(self, link):
+        self.link = link
+        self.title = ""
+    def set_title(self, title):
+        self.title = title
+
 url = "https://news.ycombinator.com"
 page = urlopen(url)
 html = page.read().decode("utf-8")
 
-pattern = "a href=.*?titlelink.*?</a>"
-match_titles = re.findall(pattern, html, re.IGNORECASE)
-for i in match_titles:
+foundThings = []
+
+patternlinks = "a href=.*?titlelink.*?</a>"
+match_links = re.findall(patternlinks, html, re.IGNORECASE)
+for i in match_links:
     i = re.search("\".*?\"", i, re.IGNORECASE)
-    print(i.group().strip('"'))
+    theLink = i.group().strip('"')
+    thing = FoundThing(theLink)
+    foundThings.append(thing)
+
+patterntitles = "titlelink\"\>.*?\<"
+match_titles = re.findall(patterntitles, html, re.IGNORECASE)
+f = 0
+for j in match_titles:
+    j = re.search(">.*?<", j, re.IGNORECASE)
+    theTitle = j.group().strip('<').strip('>')
+    foundThings[f].set_title(theTitle)
+    f+= 1
+    
+for thing in foundThings:
+    print(thing.title + ": " + thing.link)
+    
